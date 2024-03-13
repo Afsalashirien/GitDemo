2f4283bd11d7a9c76b346d9e1ef4907afcbe0072
afsala <afsalashirien@gmail.com>
2024-03-13 10:00:08 +0400

    first commit

---
+++ src/Appium.java
@@ -0,0 +1,12 @@
+
+public class Appium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is learning3");
+		System.out.println("this is learning4");
+		
+	}
+
+}
---
+++ src/Selenium.java
@@ -0,0 +1,13 @@
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is learning");
+		System.out.println("this is learning2");
+		
+		
+	}
+
+}
